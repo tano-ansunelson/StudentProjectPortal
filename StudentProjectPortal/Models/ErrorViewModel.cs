7a207cbd619367f45bede0f2080337b09e65a931
Nelson <tanoansunelson@gmail.com>
2025-12-04 15:37:12 +0000

    Add project files.

---
+++ StudentProjectPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentProjectPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
